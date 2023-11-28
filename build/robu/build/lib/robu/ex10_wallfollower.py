56f0356070f1611b378464c67bc50acaa3a73e0e
muelen19~ <muelen19@htl-kaindorf.at~>
2023-11-28 12:14:23 +0100

    Wallfollower print auskommentiert

--- build/robu/build/lib/robu/ex10_wallfollower.py
+++ build/robu/build/lib/robu/ex10_wallfollower.py
@@ -27,6 +27,7 @@ ROBOT_DIRECTION_LEFT_FRONT_INDEX = 45
 
 #Zustände definieren..........................................................................................
 class WallFollowerStates(IntEnum):
+    WF_STATE_INVALID = -1,
     WF_STATE_DETECTWALL = 0,
     WF_STATE_DRIVE2WALL = 1,
     WF_STATE_ROTATE2WALL = 2,
@@ -52,7 +53,7 @@ class WallFollower(Node):
         self.rear_dist = 999999.9
         self.distances = []
 
-        self.wallfollower_state = WallFollowerStates.WF_STATE_DETECTWALL
+        self.wallfollower_state = WallFollowerStates.WF_STATE_INVALID
 
         self.forward_speed_wf_slow = 0.05 #m/s
         self.forward_speed_wf_fast = 0.1  #m/s
@@ -74,7 +75,7 @@ class WallFollower(Node):
             self.follow_wall()
 
 
-    #Regler, State
+    #Regler, State-Maschine
     def follow_wall(self):
 
         #neue Nachricht anlegen
@@ -88,9 +89,16 @@ class WallFollower(Node):
         msg.angular.y = 0.0
         msg.angular.z = 0.0
 
+
+        #State Invalid
+        if self.wallfollower_state == WallFollowerStates.WF_STATE_INVALID:
+            print("WF_STATE_DETECTWALL")
+            self.wallfollower_state = WallFollowerStates.WF_STATE_DETECTWALL
+
+
         #State Detectwall
-        if self.wallfollower_state == WallFollowerStates.WF_STATE_DETECTWALL:
-            print("\nDetectwall\n")
+        elif self.wallfollower_state == WallFollowerStates.WF_STATE_DETECTWALL:
+            print("\nDetect Wall\n")
             dist_min = min(self.distances)
             if self.front_dist > dist_min:
                 #wenn er knapp bei der wand ist dreh langsam
@@ -105,10 +113,84 @@ class WallFollower(Node):
                 print("WF_STATE_DRIVE2Wall")
                 self.wallfollower_state = WallFollowerStates.WF_STATE_DRIVE2WALL
 
+
+        #State Drive2Wall
+        elif self.wallfollower_state == WallFollowerStates.WF_STATE_DRIVE2WALL:
+            print("\nDrive to Wall\n")
+            fd_thresh = self.dist_thresh_wf + self.dist_laser_offset
+            forward_speed_wf = self.calc_linear_speed()
+            if self.front_dist > (fd_thresh + self.dist_hysteresis_wf):
+                msg.linear.x = forward_speed_wf
+            elif self.front_dist < (fd_thresh - self.dist_hysteresis_wf):
+                msg.linear.x = -forward_speed_wf
+            else:
+                turn_direction = self.align_front()
+                msg.angular.z = self.turning_speed_wf_slow * turn_direction
+                if turn_direction == 0:
+                    print("WF_STATE_ROTATE2WALL")
+                    #safe the current distances as input for the state ROTATE2WALL
+                    self.wallfollower_state_input_dist = self.distances
+                    self.wallfollower_state = WallFollowerStates.WF_STATE_ROTATE2WALL
+
+
+        #State Rotate2Wall
+        elif self.wallfollower_state == WallFollowerStates.WF_STATE_ROTATE2WALL:
+            print("\nRotate to Wall\n")
+            sr = self.wallfollower_state_input_dist[ROBOT_DIRECTION_RIGHT_INDEX]
+            if((sr != np.inf) and (abs(self.front_dist - self.dist_laser_offset - sr) > 0.05)) or ((self.front_dist !=  np.inf) and (sr ==np.inf)):
+                msg.angular.z = -self.turning_speed_wf_fast
+            else:
+                turn_direction = self.align_left()
+                msg.angular.z = self.turning_speed_wf_slow * turn_direction
+                if turn_direction == 0:
+                    print("WF_STATE_FOLLOWWALL")
+                    self.wallfollower_state = WallFollowerStates.WF_STATE_FOLLOWWALL
+
+        #State FollowWall
+        elif self.wallfollower_state == WallFollowerStates.WF_STATE_FOLLOWWALL:
+            print("\nFollow Wall\n")
+
+
+
         #Geschwindigkeiten rausschicken
+        print(msg)
         self.cmd_vel_publisher.publish(msg)
 
+
+    #Methode um Geschwindigkeit (langsam oder schnell) zu berechnen
+    def calc_linear_speed(self):
+        fd_thresh = self.dist_thresh_wf + self.dist_laser_offset
+        if self.front_dist > ( 1.2 * fd_thresh):
+            forward_speed_wf = self.forward_speed_wf_fast
+        else:
+            forward_speed_wf = self.forward_speed_wf_slow
+
+        return forward_speed_wf
     
+
+    #Methode um gerade zu Wand ausrichten
+    def align_front(self):
+        fl = self.distances[ROBOT_DIRECTION_LEFT_FRONT_INDEX]
+        fr = self.distances[ROBOT_DIRECTION_RIGHT_FRONT_INDEX]
+        if (fl - fr) > self.dist_hysteresis_wf:
+            return 1 #turning left
+        elif (fr - fl) > self.dist_hysteresis_wf:
+            return -1 #turning right
+        else:
+            return 0 #aligned
+        
+    #Methode um paralell zu Wand ausrichten
+    def align_left(self):
+        fl = self.distances[ROBOT_DIRECTION_LEFT_FRONT_INDEX]
+        rl = self.distances[ROBOT_DIRECTION_LEFT_REAR_INDEX]
+        if (fl - rl) > self.dist_hysteresis_wf:
+            return 1 #turning left
+        elif (rl - fl) > self.dist_hysteresis_wf:
+            return -1 #turning right
+        else:
+            return 0 #aligned
+
+
     #Scan Callback, Lidar Daten holen
     def scan_callback(self, msg):
 
